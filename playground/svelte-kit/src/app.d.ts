ae4b6f69334fafc0cee62bfe4749b74fd6a0fc9e
bluwy <bjornlu.dev@gmail.com>
2022-08-06 23:41:01 +0800

    Format code

--- playground/svelte-kit/src/app.d.ts
+++ playground/svelte-kit/src/app.d.ts
@@ -2,10 +2,10 @@
 // for information about these interfaces
 // and what to do when importing types
 declare namespace App {
-	// interface Locals {}
-	// interface Platform {}
-	// interface PrivateEnv {}
-	// interface PublicEnv {}
-	// interface Session {}
-	// interface Stuff {}
+  // interface Locals {}
+  // interface Platform {}
+  // interface PrivateEnv {}
+  // interface PublicEnv {}
+  // interface Session {}
+  // interface Stuff {}
 }
