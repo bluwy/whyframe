9cdef9fd534ec2c8920619aad074609ebe1360a1
bluwy <bjornlu.dev@gmail.com>
2022-08-08 14:37:00 +0800

    Fix vitepress build setup

--- playground/vitepress/.vitepress/config.js
+++ playground/vitepress/.vitepress/config.js
@@ -1,6 +1,5 @@
-import { defineConfig, } from 'vitepress'
+import { defineConfig } from 'vitepress'
 
 export default defineConfig({
-  title: 'whyframe',
-  description: 'Why??'
+  title: 'whyframe'
 })
