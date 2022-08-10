8bd8fb595f18728f3a45cf30df99b88b1cd79335
bluwy <bjornlu.dev@gmail.com>
2022-08-10 14:54:48 +0800

    Enable all the astro integrations

--- playground/astro/astro.config.js
+++ playground/astro/astro.config.js
@@ -1,11 +1,18 @@
 import { defineConfig } from 'astro/config'
 import svelte from '@astrojs/svelte'
+import vue from '@astrojs/vue'
+import solid from '@astrojs/solid-js'
+import preact from '@astrojs/preact'
+import react from '@astrojs/react'
 import inspect from 'vite-plugin-inspect'
 import { whyframe } from '@whyframe/core'
 import { whyframeAstro } from '@whyframe/astro'
+import { whyframeSvelte } from '@whyframe/svelte'
+import { whyframeVue } from '@whyframe/vue'
+import { whyframeJsx } from '@whyframe/jsx'
 
 export default defineConfig({
-  integrations: [svelte()],
+  integrations: [svelte(), vue(), solid(), preact(), react()],
   vite: {
     plugins: [
       inspect(),
@@ -14,7 +21,24 @@ export default defineConfig({
           basic: '/frames/basic'
         }
       }),
-      whyframeAstro()
+      whyframeAstro({
+        defaultFramework: 'svelte'
+      }),
+      whyframeSvelte(),
+      whyframeVue(),
+      // what mad man
+      whyframeJsx({
+        include: /\.solid\.jsx$/,
+        framework: 'solid'
+      }),
+      whyframeJsx({
+        include: /\.preact\.jsx$/,
+        framework: 'preact'
+      }),
+      whyframeJsx({
+        include: /\.react\.jsx$/,
+        framework: 'react'
+      })
     ]
   }
 })
