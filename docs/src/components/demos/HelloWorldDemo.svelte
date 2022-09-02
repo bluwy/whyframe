668c763ed7dd6e8a8cc6668903a7a758119f1f63
bluwy <bjornlu.dev@gmail.com>
2022-09-02 15:45:39 +0800

    Add features demo

---
+++ docs/src/components/demos/HelloWorldDemo.svelte
@@ -0,0 +1,20 @@
+<!-- prettier-ignore -->
+<iframe data-why title="Package demo">
+  Hello world
+</iframe>
+
+<style>
+  iframe {
+    width: 100%;
+    border: 0;
+    height: 80px;
+    border-radius: 0.5rem;
+  }
+
+  @media (min-width: 768px) {
+    iframe {
+      width: calc(100% + 2rem);
+      margin: 0 -1rem;
+    }
+  }
+</style>
