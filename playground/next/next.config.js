9a4379f8aea26549bc88051b83f46e75b0c33b49
bluwy <bjornlu.dev@gmail.com>
2022-09-28 15:40:37 +0800

    Add next playground

---
+++ playground/next/next.config.js
@@ -0,0 +1,26 @@
+import { WhyframePlugin } from '@whyframe/core/webpack'
+
+/** @type {import('next').NextConfig} */
+export default {
+  webpack(config) {
+    config.plugins.unshift(
+      new WhyframePlugin({
+        defaultSrc: '/frames/basic',
+        components: [{ name: 'Story', showSource: true }]
+      })
+    )
+    config.module.rules.push({
+      test: /\.jsx$/,
+      exclude: /node_modules/,
+      use: [
+        {
+          loader: '@whyframe/jsx/loader',
+          options: {
+            defaultFramework: 'react'
+          }
+        }
+      ]
+    })
+    return config
+  }
+}
---
+++ playground/next/pages/index.jsx
@@ -0,0 +1,45 @@
+import Head from 'next/head'
+import { Counter } from '../components/Counter.jsx'
+import { Story } from '../components/Story.jsx'
+
+export default function Home() {
+  const max = 10
+
+  function warn() {
+    // NOTE: will affect callee's iframe, not this parent document
+    console.log('warn!')
+  }
+
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>whyframe next</title>
+      </Head>
+
+      <div id="app">
+        <h1>React</h1>
+
+        <iframe data-why title="Hello">
+          <p>Click to increment!</p>
+          <Counter />
+        </iframe>
+
+        <iframe data-why title="World" src="/frames/basic">
+          <p>Do not go over {max}</p>
+          <Counter max={max} onMax={warn} />
+        </iframe>
+
+        <Story title="Hello">
+          <p>Click to increment!</p>
+          <Counter />
+        </Story>
+
+        <Story title="World" src="/frames/basic">
+          <p>Do not go over {max}</p>
+          <Counter max={max} onMax={warn} />
+        </Story>
+      </div>
+    </>
+  )
+}
