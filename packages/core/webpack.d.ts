4e4156fea7a0cf880cbbc2a4c4a2a19fe49615e9
bluwy <bjornlu.dev@gmail.com>
2022-09-29 01:23:09 +0800

    Fix core webpack types

--- packages/core/webpack.d.ts
+++ packages/core/webpack.d.ts
@@ -1,3 +1,4 @@
+import type { Compiler } from 'webpack'
 import type { Options as _Options } from './index.js'
 
 export interface Options extends Omit<_Options, 'include' | 'exclude'> {}
@@ -5,4 +6,5 @@ export interface Options extends Omit<_Options, 'include' | 'exclude'> {}
 // also implements `Api`
 export class WhyframePlugin {
   constructor(options?: Options) {}
+  apply: (compiler: Compiler) => void
 }
