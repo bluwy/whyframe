c91166e40fbc851c05b97c9cf3a746c9ba187136
bluwy <bjornlu.dev@gmail.com>
2022-10-06 11:16:25 +0800

    Fix docusaurus build

--- playground/docusaurus/docusaurus.config.js
+++ playground/docusaurus/docusaurus.config.js
@@ -118,10 +118,6 @@ const config = {
             title: 'More',
             items: [
               {
-                label: 'Blog',
-                to: '/blog'
-              },
-              {
                 label: 'GitHub',
                 href: 'https://github.com/facebook/docusaurus'
               }
