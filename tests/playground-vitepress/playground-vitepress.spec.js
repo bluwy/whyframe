e865ca65f9e058b4641410fa58846c88145f3c1e
bluwy <bjornlu.dev@gmail.com>
2022-10-08 16:04:21 +0800

    Add more tests

---
+++ tests/playground-vitepress/playground-vitepress.spec.js
@@ -0,0 +1,9 @@
+import { test, expect } from '@playwright/test'
+import { setup } from '../testUtils.js'
+
+setup()
+
+test('render the page', async ({ page }) => {
+  await page.goto('/')
+  expect(await page.locator('h1').textContent()).toBe('VitePress #')
+})
