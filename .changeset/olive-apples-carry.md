03ffc063e6ee3c3f76ade1480a5bc415adb986df
bluwy <bjornlu.dev@gmail.com>
2025-03-18 11:43:00 +0800

    Support Vite 6 peer dep
    fix #38

---
+++ .changeset/olive-apples-carry.md
@@ -0,0 +1,8 @@
+---
+'@whyframe/svelte': patch
+'@whyframe/astro': patch
+'@whyframe/jsx': patch
+'@whyframe/vue': patch
+---
+
+Support Vite 6 in peer dependencies range
--- scripts/fix-versions.js
+++ scripts/fix-versions.js
@@ -6,17 +6,43 @@ import path from 'node:path'
 // pnpm likes to convert them to `workspace:*` when updating, this scripts reverts it,
 // and can be helpful when releasing new packages.
 
+// peer deps are also fixed to the base version so that it doesn't change between releases
+
 // find all package versions in packages/
 const packages = await fs.readdir('packages')
 const versions = {}
 for (const pkg of packages) {
   if (pkg.startsWith('.')) continue
-  const pkgJson = await fs.readFile(
-    path.join('packages', pkg, 'package.json'),
-    'utf-8'
-  )
+  const pkgJsonPath = path.join('packages', pkg, 'package.json')
+  const pkgJson = await fs.readFile(pkgJsonPath, 'utf-8')
   const pkgJsonObj = JSON.parse(pkgJson)
   versions[pkgJsonObj.name] = pkgJsonObj.version
+}
+
+// fix package peer deps
+for (const pkg of packages) {
+  if (pkg.startsWith('.')) continue
+  const pkgJsonPath = path.join('packages', pkg, 'package.json')
+  const pkgJson = await fs.readFile(pkgJsonPath, 'utf-8')
+  const pkgJsonObj = JSON.parse(pkgJson)
+  for (const dep in pkgJsonObj.peerDependencies) {
+    if (versions[dep]) {
+      // 1.2.3 -> 1.0.0
+      // 0.1.2 -> 0.1.0
+      // 0.0.1 -> 0.0.1
+      const firstNonZeroIndex = versions[dep]
+        .split('')
+        .findIndex((c) => c !== '0')
+      const baseVersionArray = []
+      for (let i = 0; i < 3; i++) {
+        baseVersionArray.push(
+          i === firstNonZeroIndex ? versions[dep].split('.')[i] : '0'
+        )
+      }
+      pkgJsonObj.peerDependencies[dep] = `^${baseVersionArray.join('.')}`
+    }
+  }
+  await fs.writeFile(pkgJsonPath, JSON.stringify(pkgJsonObj, null, 2) + '\n')
 }
 
 // find all playground projects, since the versions with ^
